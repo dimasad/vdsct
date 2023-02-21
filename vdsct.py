724011b5c8cb3e53a75e59093753f85b7264268c
Dimas Abreu Archanjo Dutra <dimas.dutra@mail.wvu.edu>
2023-02-21 16:38:53 -0500

    Added subject and segment name and count.

--- vdsct.py
+++ vdsct.py
@@ -26,8 +26,23 @@ class Client:
     def get_subject_count(self) -> int:
         return _client_getsubjectcount(self._client_p)
 
+    def get_subject_name(self, index: int) -> bytes:
+        return _client_getsubjectname(self._client_p, index)
+    
     def get_segment_count(self, subject: bytes | str) -> int:
         return _client_getsegmentcount(self._client_p, subject)
+    
+    def get_segment_name(self, subject: bytes | str, index: int) -> bytes:
+        return _client_getsegmentname(self._client_p, subject, index)
+
+    def iter_segments(self):
+        nsubj = self.get_subject_count()
+        for i in range(nsubj):
+            subj = self.get_subject_name(i)
+            nseg = self.get_segment_count(subj)
+            for j in range(nseg):
+                seg = self.get_segment_name(subj, j)
+                yield (subj, seg)
 
 
 class Output_GetSubjectCount(ctypes.Structure):
@@ -49,14 +64,17 @@ def load_library(path=None):
     _so = ctypes.cdll.LoadLibrary(path)
 
     # Configure the functions
-    _so.Client_Create.restype = ctypes.c_void_p
-    _so.Client_Connect.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-    _so.Client_Destroy.argtypes = [ctypes.c_void_p]
+    from ctypes import c_void_p, c_int, c_char_p
+    _so.Client_Create.restype = c_void_p
+    _so.Client_Connect.argtypes = [c_void_p, c_char_p]
+    _so.Client_Destroy.argtypes = [c_void_p]
     _so.Client_Destroy.restype = None
-    _so.Client_GetFrame.argtypes = [ctypes.c_void_p]
-    _so.Client_GetSubjectCount.argtypes = [ctypes.c_void_p, ctypes.c_void_p]
-    _so.Client_GetSegmentCount.argtypes = [
-        ctypes.c_void_p,  ctypes.c_char_p, ctypes.c_void_p
+    _so.Client_GetFrame.argtypes = [c_void_p]
+    _so.Client_GetSubjectCount.argtypes = [c_void_p, c_void_p]
+    _so.Client_GetSubjectName.argtypes = [c_void_p, c_int, c_int, c_char_p]
+    _so.Client_GetSegmentCount.argtypes = [c_void_p,  c_char_p, c_void_p]
+    _so.Client_GetSegmentName.argtypes = [
+        c_void_p, c_char_p, c_int, c_int, c_char_p
     ]
 
 
@@ -64,6 +82,11 @@ def so():
     if _so is None:
         load_library()
     return _so
+
+
+def assert_success(code):
+    if code != SUCCESS_CODE:
+        raise RuntimeError(RESULTS[code])
 
 
 def _client_create():
@@ -86,24 +109,28 @@ def _client_connect(client_p, host: bytes | str):
         host = host.encode()
 
     r = so().Client_Connect(client_p, host)
-    if Result[r] != 'Success':
-        raise RuntimeError(Result[r])
+    assert_success(r)
 
 
 def _client_getframe(client_p):
     """Low-level interface to ViconDataStreamSDK_C Client_GetFrame"""
     r = so().Client_GetFrame(client_p)
-    if Result[r] != 'Success':
-        raise RuntimeError(Result[r])
+    assert_success(r)
 
 
 def _client_getsubjectcount(client_p):
     """Low-level interface to ViconDataStreamSDK_C Client_GetSubjectCount"""
     out = Output_GetSubjectCount()
     so().Client_GetSubjectCount(client_p, ctypes.byref(out))
-    if Result[out.Result] != 'Success':
-        raise RuntimeError(Result[out.Result])
+    assert_success(out.Result)
     return out.SubjectCount
+
+
+def _client_getsubjectname(client_p, index):
+    name = ctypes.create_string_buffer(128)
+    r = so().Client_GetSubjectName(client_p, index, len(name), name)
+    assert_success(r)
+    return name.value
 
 
 def _client_getsegmentcount(client_p, subject):
@@ -114,9 +141,15 @@ def _client_getsegmentcount(client_p, subject):
 
     out = Output_GetSegmentCount()
     so().Client_GetSegmentCount(client_p, subject, ctypes.byref(out))
-    if Result[out.Result] != 'Success':
-        raise RuntimeError(Result[out.Result])
+    assert_success(out.Result)
     return out.SegmentCount
+
+
+def _client_getsegmentname(client_p, subject, index):
+    name = ctypes.create_string_buffer(128)
+    r = so().Client_GetSegmentName(client_p, subject, index, len(name), name)
+    assert_success(r)
+    return name.value
 
 
 @contextlib.contextmanager
@@ -131,8 +164,10 @@ def client():
 _so = None
 """DataStreamSDK library shared object."""
 
+SUCCESS_CODE = 2
+"""Success code in the Vicon CEnum."""
 
-Result = [
+RESULTS = [
     'Unknown',
     'NotImplemented',
     'Success',
@@ -162,4 +197,4 @@ Result = [
     'ConfigurationFailed',
     'NotPresent',
 ]
-"""ViconDataStreamSDK result codes enum."""
+"""ViconDataStreamSDK result codes enum (CEnum)."""
